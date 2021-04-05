1dcfa7e80fba7300631f37df898b8b22a5171c1b
Akın Ayhan <>
2021-04-05 14:26:16 +0000

    Initial commit

---
+++ degiskenler.dart
@@ -0,0 +1,9 @@
+void main() {
+String mesaj = "hello world";
+int dogumYili=1994;
+double oran = 1.15;
+bool loginmi =false;
+	print(mesaj);
+  print("Doğum Tarihi : "+dogumYili.toString());
+  print("Oran : "+oran.toString());
+}
