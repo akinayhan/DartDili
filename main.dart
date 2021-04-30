967d656f64877645f0f4435cc750e3cce1307471
Akın Ayhan <>
2021-04-30 15:18:28 +0000

    30.04

--- main.dart
+++ main.dart
@@ -1,90 +1,29 @@
 void main(){
-  var personelManager = PersonelManager();
-  personelManager.add();
 
-  var customerManager = CustomerManager();
-  Person customer1 =new Customer.withInfo("AKIN","AYHAN");
+//type safety
+  List<String> sehirler = new List<String>(3);
+  sehirler[0]="Ankara";
+  sehirler[1]="İstanbul";
+  sehirler[2]="İzmir";
 
-  var customer2 = new Customer();
-  customer2.firstName ="Akın";
-  customer2.lastname = "Ayhan";
+  print(sehirler);
 
-  customer1 = customer2;
-  customer2.firstName = "Ahmet";
+  List<String> urunler = ["Laptop","Mause", "Keyboard"];
+  print(urunler);
 
-  customerManager.add(customer1);
+  var product1 = Product("TV",500);
+  var product2 = Product("Mikrodalga Fırın",650);
+  List<Product> products = [product1,product2];
 
-  Personel personel1 = Personel();
-  personel1.firstName = "Ali";
-
-  var controller = PersonController();
-  controller.operation(personel1);
+  print(products[0].name + " " + products[0].unitPrice.toString());
 }
 
+class Product{
+  String name;
+  double unitPrice;
 
-class PersonController{
-  void operation(Person person){
-    print("Inheritance demo:" + person.firstName);
+  Product(String name, double unitPrice){
+    this.name = name;
+    this.unitPrice = unitPrice;
   }
 }
-
-class PersonelManager{
-  void add(){
-    print("Eklendi!");
-  }
-
-  void update(){
-    print("Güncellendi");
-  }
-
-  void delete(){
-    print("Silindi");
-  }
-}
-
-class CustomerManager{
-  void add(Customer customer){
-    print("Eklendi : "+ customer.firstName);
-  }
-
-  void update(){
-    print("Güncellendi");
-  }
-
-  void delete(){
-    print("Silindi");
-  }
-}
-
-class Customer extends Person{ 
-
-    Customer(){
-      
-    }
-    //constructor
-    Customer.withInfo(String firstName, String lastname){
-      this.firstName = firstName;
-      this.lastname = lastname;
-    }
-  }
-
-  class Personel extends Person{ 
-    int dateOfStart;
-
-    Personel(){
-      
-    }
-    //constructor
-    Personel.withInfo(String firstName, String lastname, int dateOfStart){
-      this.firstName = firstName;
-      this.lastname = lastname;
-      this.dateOfStart = dateOfStart;
-    }
-  }
-
-  class Person {
-    String firstName;
-    String lastname;
-    String identityNumber;
-  
-  }
